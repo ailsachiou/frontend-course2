b296478c977a8a51401f686726484c98865ea0f1
ailsachiou <ailsachiou@gmail.com>
2022-11-10 01:13:11 +0800

    renew words in max-of-two-numbers

--- Flow-Control-Assignment/max-of-two-numbers.js
+++ Flow-Control-Assignment/max-of-two-numbers.js
@@ -1,11 +1,11 @@
 // 比大小，給定兩個數，並找出哪個數字比較大
-let num1 = 5;
-let num2 = 3;
+let num1 = 49;
+let num2 = 50;
 
 if (num1 > num2) {
-  console.log(num1, "數字1比較大");
+    console.log("num1比較大：", num1);
 } else if (num2 > num1) {
-  console.log(num2, "數字2比較大");
+    console.log("num2比較大：", num2);
 } else {
-  console.log("兩個數字一樣大");
+    console.log("兩個數字一樣大");
 }
